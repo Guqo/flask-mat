465e95b8cd680fda48735922a1c63941d4a1d0ba
Guqo <ryto04@seznam.cz>
2023-10-20 13:12:08 +0200

    Login, register

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, redirect, render_template, url_for, request, g, session
 from database import connect_to_database, getDatabase
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.teardown_appcontext
 def close_database(error):
@@ -20,30 +21,57 @@ def get_current_user():
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    user = get_current_user()
+    return render_template("home.html", user = user)
 
-@app.route("/login")
+@app.route("/login", methods = ['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    user = get_current_user()
+    error = None
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        db = getDatabase()
+        fetchedperson_cursor = db.execute("select * from users table where name = ?", [name])
+        personfromdatabase = fetchedperson_cursor.fetchone()
+        if personfromdatabase:
+            if check_password_hash(personfromdatabase['password'], password):
+                session['user'] = personfromdatabase['name']
+                return redirect(url_for('index'))
+            else:
+                error = "Username or password did not match. Try again."
+                return render_template('login.html', error = error)
+        else:
+            error = "Username or password did not match. Try again."
+            return redirect(url_for('login'))
+
+    return render_template("login.html", user = user, error = error)
 
 @app.route("/register", methods = ["POST", "GET"])
 def register():
+    user = get_current_user()
+    error = None
     if request.method == "POST":
         db = getDatabase()
         name = request.form["name"]
         password = request.form["password"]
-        hashed_password = generate_password_hash(password, method='sha256')
-
+        user_fetcing_cursor = db.execute("select * from users where name = ?", [name])
+        existing_user  = user_fetcing_cursor.fetchone()
+        if existing_user:
+            error = "Username already taken, please choose a different username."
+            return render_template("register.html", error = error)
+        hashed_password = generate_password_hash(password)
         db.execute("insert into users (name, password, teacher, admin) values (?,?,?,?)",
                     [name, hashed_password, '0', '0'])
         db.commit()
+        session['user'] = name
         return redirect(url_for('index'))
-
-    return render_template("register.html")
+    return render_template("register.html", user = user)
 
 @app.route("/logout")
 def logout():
-    return render_template("home.html")
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug = True)
