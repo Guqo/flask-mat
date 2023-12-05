b250559a9d8a8054a48bde8a23895c7694356888
Tomáš <77784708+Guqo@users.noreply.github.com>
2023-12-05 14:20:41 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import g
 
 def connect_to_database():
-    sql = sqlite3.connect('C:/Users/ryto0/Desktop/flask-mat-main/flaskapp.db')
+    sql = sqlite3.connect('../flask-mat-main/flaskapp.db')
     sql.row_factory = sqlite3.Row
     return sql
 
